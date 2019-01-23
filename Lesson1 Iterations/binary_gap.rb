4bd87195bfb85d23bd8e6d3cd9319cacf9f6d5c9
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-01-23 21:32:21 +0900

    正規表現を使って動くバージョン

--- Lesson1 Iterations/binary_gap.rb
+++ Lesson1 Iterations/binary_gap.rb
@@ -8,14 +8,19 @@ https://app.codility.com/programmers/lessons/1-iterations/binary_gap/
 require 'minitest/autorun'
 
 def solution(n)
-  return 0
+  # 1に囲まれている0を正規表現で探す
+  if /1(?<zero_seq>0+)1/ =~ n.to_s(2)
+    zero_seq.to_s.length
+  else
+    0
+  end
 end
 
 class BinaryGapTest < Minitest::Test
   def test_solution
     assert_equal 2, solution(9)   # 1001
     assert_equal 4 ,solution(529) # 1000010001
-    assert_eaual 1, solution(20)  # 10100
+    assert_equal 1, solution(20)  # 10100
     assert_equal 0, solution(15)  # 1111
     assert_equal 0, solution(32)  # 100000
     assert_equal 5, solution(1041) # 10000010001
