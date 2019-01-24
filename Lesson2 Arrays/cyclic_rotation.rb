6d81212dd44aa2e180480d34ba97dd130e4fc774
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-01-24 21:05:01 +0900

    雛形を追加

---
+++ Lesson2 Arrays/cyclic_rotation.rb
@@ -0,0 +1,21 @@
+=begin
+CyclicRotation
+Rotate an array to the right by a given number of steps.
+
+https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
+=end
+
+require 'minitest/autorun'
+
+def solution(a, k)
+  return a
+end
+
+class CyclicRotationTest < Minitest::Test
+  def test_solution
+    assert_equal [6, 3, 8, 9, 7], solution([3, 8, 9, 7, 6], 1)
+    assert_equal [9, 7, 6, 3, 8], solution([3, 8, 9, 7, 6], 3)
+    assert_equal [0, 0, 0], solution([0, 0, 0], 1)
+    assert_equal [1, 2, 3, 4], solution([1, 2, 3, 4], 4)
+  end
+end
