41b61aee31cffad3bd6dcb15a0306fde87216892
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-01-26 09:05:11 +0900

    Minitestで出題に関するテストを用意

---
+++ Lesson3 TimeComplexity/frog_jmp.rb
@@ -0,0 +1,16 @@
+=begin
+FrogJmp
+Count minimal number of jumps from position X to Y.
+=end
+
+require 'minitest/autorun'
+
+def solution(x, y, d)
+  return 3
+end
+
+class FrogJmpTest < Minitest::Test
+  def test_solution
+    assert_equal 3, solution(10, 85, 30)
+  end
+end
