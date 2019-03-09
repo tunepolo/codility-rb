929944fb77373a7881b0bc54c54ac51d5c70a223
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-03-09 20:13:02 +0900

    簡単にコメントを追加

--- Lesson4_CountingElements/max_counters.rb
+++ Lesson4_CountingElements/max_counters.rb
@@ -8,29 +8,30 @@ require 'minitest/autorun'
 
 def solution(n, a)
   counter = Array.new(n, 0)
-  current_max_value = 0
-  last_reset_max_value = 0
-  reset_value = n+1
+  current_max_value = 0     # 集計中のカウンタ最大値
+  last_reset_max_value = 0  # 最後にカウンタリセットがあった時の値
+  reset_value = n+1         # リセット指示となる入力値
 
   a.each do |value|
     counter_index = value - 1
 
-    if value != reset_value
+    if value == reset_value
+      last_reset_max_value = current_max_value
+    else
+      # カウンタ値が最後のリセットよりも少なければまず更新
       if counter[counter_index] < last_reset_max_value
         counter[counter_index] = last_reset_max_value
       end
 
+      # カウンタ値の更新
       counter[counter_index] += 1
       if counter[counter_index] > current_max_value
         current_max_value = counter[counter_index]
       end
-    else
-      last_reset_max_value = current_max_value
     end
-
-     counter
   end
 
+  # 未更新のまま残ったカウンタ値を最後にカウンタリセットがあったときの値に更新
   counter.each_with_index do |value, index|
     if value < last_reset_max_value
       counter[index] = last_reset_max_value
