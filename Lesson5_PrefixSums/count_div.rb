5ea5e249b37bba94e8312e342f15bada6b5eb330
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-04-20 10:26:17 +0900

    最初に割り切れる数を見つけて倍数を数えるやり方に修正

--- Lesson5_PrefixSums/count_div.rb
+++ Lesson5_PrefixSums/count_div.rb
@@ -6,17 +6,27 @@ Compute number of integers divisible by k in range [a..b].
 require 'minitest/autorun'
 
 def solution(a, b, k)
-  count = 0
+  first_value = -1
 
-  (a..b).each do |val|
-    count += 1 if val % k == 0
+  # 割り切れる最初の数を見つける
+  (a..b).each do |value|
+    if value % k == 0
+      first_value = value
+      break
+    end
   end
 
-  return count
+  # 割り切れる数が見つからなければ0を返す
+  return 0 if first_value < 0
+
+  # first_valからbまでにいくつkが入るかを数える
+  return ((b - first_value) / k ) + 1
 end
 
 class CountDivTest < Minitest::Test
   def test_solution
     assert_equal 3, solution(6, 11, 2)
+    assert_equal 1, solution(10, 10, 5)
+    assert_equal 0, solution(10, 10, 20)
   end
 end
