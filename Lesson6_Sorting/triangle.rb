5c6ef616bb64b7c89f6f49e0699a91fec98d04e7
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-04-23 16:28:19 +0900

    素直に解いてみた

---
+++ Lesson6_Sorting/triangle.rb
@@ -0,0 +1,24 @@
+=begin
+Triangle
+Determine whether a triangle can be built from a given set of edges.
+=end
+
+require 'minitest/autorun'
+
+def solution(a)
+  a.combination(3).each do |arr|
+    p, q, r = arr
+    if p + q > r && q + r > p && r + p > q then
+      return 1
+    end
+  end
+
+  return 0
+end
+
+class TriangleTest < Minitest::Test
+  def test_solution
+    assert_equal 1, solution([10, 2, 5, 1, 8, 20])
+    assert_equal 0, solution([10, 50, 5, 1])
+  end
+end
