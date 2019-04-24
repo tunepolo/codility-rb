fd51e222ee86205ac4768c69610433c7638669a1
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-04-25 08:53:58 +0900

    生存区間を調べる方法で解いた。
    参考にしたサイト
    http://codility-lessons-jp.blogspot.com/2015/02/lesson-4-numberofdiscintersections.html

--- Lesson6_Sorting/number_of_disc_intersections.rb
+++ Lesson6_Sorting/number_of_disc_intersections.rb
@@ -6,20 +6,36 @@ Compute the number of intersections in a sequence of discs.
 require 'minitest/autorun'
 
 def solution(a)
-  count = 0
-  count_limit = 10_000_000
-
-  (0...a.size).to_a.combination(2).each do |comb|
-    if (comb[1]-comb[0]) <= (a[comb[0]]+a[comb[1]]) then
-      count += 1
-    end
+  # 生存区間を生成
+  # keyが円の中心座標、startが円の左端、endが円の右端
+  circle_stat = Hash.new{ |h, k| h[k] = { start: 0, end: 0 } }
+  a.each_with_index do |radius, index|
+    circle_stat[index - radius][:start] += 1
+    circle_stat[index + radius][:end] += 1
   end
 
-  (count <= 10_000_000 ) ? count : -1
+  # 生存区間をソートし、active数から交差する円の数を数える
+  count = 0
+  active = 0
+  Hash[circle_stat.sort].each do |index, stat|
+    # 生存区間の終了に合わせてactiveな数を増やす
+    active += stat[:start]
+    stat[:end].times do
+      active -= 1
+      count += active
+
+      return -1 if count > 10_000_000
+    end
+
+    # puts "index:#{index} ,stat:#{stat}, active:#{active}, count:#{count}"
+  end
+
+  count
 end
 
 class NumberOfDiscIntersectionsTest < Minitest::Test
   def test_solution
     assert_equal 11, solution([1, 5, 2, 1, 4, 0])
+    assert_equal 3, solution([1, 1, 1])
   end
 end
