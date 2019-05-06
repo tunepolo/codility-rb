42cfecd039564a1d2490529616d7977d828d994b
Yuichi TSUNEMATSU <tunepolo@gmail.com>
2019-05-07 07:30:44 +0900

    平方根の判定処理で間違うことがあったので修正

--- Lesson10_PrimeAndCompositeNumbers/CountFactors.rb
+++ Lesson10_PrimeAndCompositeNumbers/CountFactors.rb
@@ -8,21 +8,22 @@ require 'complex'
 
 def solution(n)
   factor_count = 0
+  n_sqrt = Math.sqrt(n)
 
-  1.upto(Math.sqrt(n)) do |v|
-    if n % v == 0
-      factor_count += 2
-    end
+  1.upto(n_sqrt) do |v|
+    factor_count += 2 if n % v == 0
   end
 
-  factor_count -= 1 if Math.sqrt(n) ** 2 == n
+  factor_count -= 1 if n_sqrt == n_sqrt.to_i
 
   factor_count
 end
 
 class CountFactorsTest < Minitest::Test
   def test_solution
-    assert_equal 8, solution(24)
     assert_equal 3, solution(25)
+    assert_equal 2, solution(41)
+
+    assert_equal 8, solution(24)
   end
 end
